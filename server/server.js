f447bf70a2e9dc2596b0929e7b48c92122b18f9c
Bhargo <amar_9909@yahoo.com>
2017-10-07 22:08:31 +0530

    repo setup

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+
+const port = process.env.PORT || 3000;
+const app = express();
+const publicPath = path.join(__dirname,'..','public');
+
+app.use(express.static(publicPath));
+
+app.listen(port,() => {console.log(`server up on port ${port}`);})
